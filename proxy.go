7321bcddc8fb941715f13e5ed9ffc2a311ff2f17
saritajoshi9389 <joshi.sar@husky.neu.edu>
2017-11-15 00:42:48 -0500

    added server key value for set

--- proxy.go
+++ proxy.go
@@ -9,74 +9,72 @@ import (
 	"strings"
 	"encoding/json"
 	"bytes"
-
 )
 import b64 "encoding/base64"
 
-type Key struct{
-    Encoding string `json:"encoding"`
-    Data     string `json:"data"`
+type Key struct {
+	Encoding string `json:"encoding"`
+	Data     string `json:"data"`
 }
-type Value struct{
-    Encoding string `json:"encoding"`
-    Data     string `json:"data"`
+type Value struct {
+	Encoding string `json:"encoding"`
+	Data     string `json:"data"`
 }
-
 
 type MyData struct {
-       Key  `json:"key"`
-       Value  `json:"value"`
+	Key  `json:"key"`
+	Value  `json:"value"`
 }
 
-type MyDatas struct{
-  dataList MyData
-  }
+type MyDatas struct {
+	dataList MyData
+}
 
 var url string
 
 //func handler(w http.ResponseWriter, r *http.Request) {
 func handler(w http.ResponseWriter, r *http.Request, total_servers int, server_list []string) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-    	w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
-	if(r.URL.Path == "/SET") {
+	w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
+	if (r.URL.Path == "/SET") {
 		client := &http.Client{}
 		contents, _ := ioutil.ReadAll(r.Body)
 		var d []MyData
 		err1 := json.Unmarshal(contents, &d)
 		if err1 != nil {
-				fmt.Printf("hiiii%s", err1)
-				os.Exit(1)
+			fmt.Printf("hiiii%s", err1)
+			os.Exit(1)
 		}
 		//fmt.Println("URL::",url)
 		server_ele := 0
 		struct_map := make(map[int][]MyData)
 		for _, elem := range d {
-			  //fmt.Println(elem.Key,elem.Value.Data,server_list[server_ele])
-			  sEnc := b64.StdEncoding.EncodeToString([]byte(elem.Key.Data))
-			  val := elem.Key.Data[0]
-			  fmt.Println("ahhhh",sEnc)
-			  temp_struct:= MyData{
-						    Key: Key{
-							    Encoding:  elem.Key.Encoding,
-							    Data: elem.Key.Data,
-						    },
-						    Value: Value{
-							    Encoding: elem.Value.Encoding,
-							    Data: elem.Value.Data,
-						    },
-						}
-			index := int(val%3)
-			struct_map[index] = append(struct_map[index],temp_struct)
-			  server_ele ++
-              }
-		i :=0
+			//fmt.Println(elem.Key,elem.Value.Data,server_list[server_ele])
+			sEnc := b64.StdEncoding.EncodeToString([]byte(elem.Key.Data))
+			val := elem.Key.Data[0]
+			fmt.Println("ahhhh", sEnc)
+			temp_struct := MyData{
+				Key: Key{
+					Encoding:  elem.Key.Encoding,
+					Data: elem.Key.Data,
+				},
+				Value: Value{
+					Encoding: elem.Value.Encoding,
+					Data: elem.Value.Data,
+				},
+			}
+			index := int(val % 3)
+			struct_map[index] = append(struct_map[index], temp_struct)
+			server_ele ++
+		}
+		i := 0
 		for i < total_servers {
-			if val , ok := struct_map[i]; ok {
+			if val, ok := struct_map[i]; ok {
 				json_obj, _ := json.Marshal(val)
 				fmt.Println(string(json_obj))
-                  		fmt.Println("temp_struct",val, i)
+				fmt.Println("temp_struct", val, i)
 				url = strings.Join([]string{"http://", "localhost:", string(server_list[i]), r.URL.Path}, "")
-				response, err := http.NewRequest(http.MethodPut,url ,bytes.NewBuffer(json_obj))
+				response, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(json_obj))
 				if err != nil {
 					os.Exit(2)
 				} else {
@@ -88,49 +86,48 @@ func handler(w http.ResponseWriter, r *http.Request, total_servers int, server_l
 						os.Exit(1)
 					}
 					fmt.Fprintf(w, string(cts))
-			}
+				}
 
 			}
 			i++
 
 		}
 
-
-	}else if (r.URL.Path == "/FETCH"){
-	//else if r.Method == "PUT"{
+	} else if (r.URL.Path == "/FETCH") {
+		//else if r.Method == "PUT"{
 		contents, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("%s", err)
-				os.Exit(1)
-			}
-			//fmt.Fprintf(w, string(contents))
-			//log.Println(url)
-		fmt.Println("abc",contents)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+		//fmt.Fprintf(w, string(contents))
+		//log.Println(url)
+		fmt.Println("abc", contents)
 		var d []MyData
 		err1 := json.Unmarshal(contents, &d)
 		if err1 != nil {
-				fmt.Printf("hiiii%s", err1)
-				os.Exit(1)
-			}
+			fmt.Printf("hiiii%s", err1)
+			os.Exit(1)
+		}
 
 		fmt.Println(d[1].Key, d[1].Value)
 
-	}else if (r.URL.Path == "/QUERY"){
-	//else if r.Method == "PUT"{
+	} else if (r.URL.Path == "/QUERY") {
+		//else if r.Method == "PUT"{
 		contents, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("%s", err)
-				os.Exit(1)
-			}
-			//fmt.Fprintf(w, string(contents))
-			//log.Println(url)
-		fmt.Println("abc",contents)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+		//fmt.Fprintf(w, string(contents))
+		//log.Println(url)
+		fmt.Println("abc", contents)
 		var d []MyData
 		err1 := json.Unmarshal(contents, &d)
 		if err1 != nil {
-				fmt.Printf("hiiii%s", err1)
-				os.Exit(1)
-			}
+			fmt.Printf("hiiii%s", err1)
+			os.Exit(1)
+		}
 
 		fmt.Println(d[1].Key, d[1].Value)
 
@@ -139,18 +136,18 @@ func handler(w http.ResponseWriter, r *http.Request, total_servers int, server_l
 
 func main() {
 	arg := os.Args[1:]
-        server_list := arg[1:]
+	server_list := arg[1:]
 
-        total_servers := len(server_list)
-        fmt.Println("oyeeeeeeeeeeeeeeeeeeeeee",total_servers,server_list)
-        if arg[0] != "-p"{
-          fmt.Println("Incorrect flag variable, exiting....")
-          return
-        }
+	total_servers := len(server_list)
+	fmt.Println("oyeeeeeeeeeeeeeeeeeeeeee", total_servers, server_list)
+	if arg[0] != "-p" {
+		fmt.Println("Incorrect flag variable, exiting....")
+		return
+	}
 	//http.HandleFunc("/", handler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-              handler(w, r, total_servers, server_list)
-       })
+		handler(w, r, total_servers, server_list)
+	})
 	fmt.Println("Proxy up and running!!!")
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
--- server.py
+++ server.py
@@ -4,16 +4,11 @@ import os
 import os.path
 import http
 import http.client
-import threading
-import queue
 import re
-import datetime
 import urllib.parse
-import logging
 import simplejson
 import json
 
-kv = {}
 
 class CustomHandler(http.server.BaseHTTPRequestHandler):
     def do_PUT(self):
@@ -28,26 +23,46 @@ class CustomHandler(http.server.BaseHTTPRequestHandler):
             print(data, "data var")
             message = simplejson.loads(data)
             print("message value ", message)
-            if(content_length != 0):
+            if (content_length != 0):
                 successfully_updated = 1
             print("Data Dir", self.server)
             print("Data Dir", self.server.data_dir)
             print("kv ", self.server.kveachinstance)
+            code = 200
+            failed_inputs = []
+            count = 0
             ########List to appending dict###########
-            for kv in message:
-                print(kv)
+            try:
+                for kv in message:
+                    print(kv["key"], kv["value"], self, self.server, self.server.kveachinstance)
+                    print("haha1.....", frozenset(kv["key"].items()))
+                    result = self.server.kveachinstance.set_value(frozenset(kv["key"].items()),
+                                                                  frozenset(kv["value"].items()))
+                    print("haha2")
+                    print(result)
+                    if not result:
+                        print("haha3")
+                        failed_inputs.append(kv["key"])
+                    else:
+                        count += 1
+                    print("result baby", self.server.kveachinstance.get_value(frozenset(kv["key"].items())))
+            except TypeError:
+                return {"error": True, "message": "Bad request"}, 400
+            except KeyError:
+                return {"error": True, "message": "Bad request"}, 400
+            if count < len(message):
+                code = 206
+            return {"Number of keys added": count, "Number of keys failed": failed_inputs}, code
 
-
-
-            #################################################
-            # respond to client
-            if successfully_updated == 1:
-                self.send_response(201)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-            else:
-                self.send_response(500, "Only {} nodes updated".format(successfully_updated))
-                self.end_headers()
+        #################################################
+        # respond to client
+        # if successfully_updated == 1:
+        #     self.send_response(201)
+        #     self.send_header("Content-type", "application/json")
+        #     self.end_headers()
+        # else:
+        #     self.send_response(500, "Only {} nodes updated".format(successfully_updated))
+        #     self.end_headers()
 
         except Exception as err:
             print(err)
@@ -65,14 +80,36 @@ class CustomHandler(http.server.BaseHTTPRequestHandler):
 
 
 class CustomHttpServer(http.server.HTTPServer):
-
-    def __init__(self, server_address, RequestHandlerClass, data_dir="data"):
+    def __init__(self, server_address, RequestHandlerClass, data_dir="data", data_map=''):
         print("Enters this server func 1")
         super(CustomHttpServer, self).__init__(server_address, RequestHandlerClass)
         self.data_dir = data_dir
         if not os.path.exists(self.data_dir): os.mkdir(self.data_dir)
-        self.kveachinstance = {}
+        self.kveachinstance = data_map
         print("exit this server func 1")
+
+
+class DataInstance:
+    def __init__(self):
+        self.data = {}
+
+    def get_value(self, key):
+        if key in self.data:
+            return self.data[key]
+        return None
+
+    def set_value(self, key, value):
+        print("enters set")
+        print(key)
+        print(value)
+        print(self)
+        self.data[key] = value
+        print(self.data.items())
+        return True
+
+    def search(self, key):
+        return key in self.data
+
 
 if __name__ == '__main__':
     print("hi")
@@ -80,14 +117,16 @@ if __name__ == '__main__':
     parser.add_option("-p", "--port", dest="port", help="local port to listen on", type="int", default=9000)
     parser.add_option("-d", "--data", dest="data_dir", help="data directory", type="string", default="data")
     (options, node_urls) = parser.parse_args()
-    httpd = CustomHttpServer(("", options.port), CustomHandler, str(options.port)+options.data_dir)
+    initial_data = DataInstance()
+    httpd = CustomHttpServer(("", options.port), CustomHandler, str(options.port) + options.data_dir, initial_data)
     print(("Server Starts - {}:{}".format("localhost", options.port)))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    folder = str(options.port)+options.data_dir
-    with open(folder+'/result.json', 'w') as fp:
-        json.dump(kv, fp)
+    folder = str(options.port) + options.data_dir
+    with open(folder + '/result.json', 'a') as fp:
+        print(initial_data.data)
+        json.dump(str(initial_data.data), fp)
     print(("Server Stops - {}:{}".format("localhost", options.port)))
