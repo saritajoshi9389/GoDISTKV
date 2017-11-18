0faa22849cec4c043802f25c2d3c04aa711d3004
akshayakhare <akshayakhare@gmail.com>
2017-11-18 04:30:23 +0000

    changes in script

--- stop_servers.sh
+++ stop_servers.sh
@@ -1,4 +1,15 @@
-pid=$(lsof -i:9000 -t); kill -TERM $pid || kill -KILL $pid
-pid1=$(lsof -i:9001 -t); kill -TERM $pid1 || kill -KILL $pid1
-pid2=$(lsof -i:9002 -t); kill -TERM $pid2 || kill -KILL $pid2
+# pid=$(lsof -i:9000 -t); kill -TERM $pid || kill -KILL $pid
+# pid1=$(lsof -i:9001 -t); kill -TERM $pid1 || kill -KILL $pid1
+# pid2=$(lsof -i:9002 -t); kill -TERM $pid2 || kill -KILL $pid2
 
+#!/bin/bash
+value=9000
+
+for number in {1..100}
+do
+temp=$(expr $value + $number)
+# echo $temp
+pid=$(lsof -i:$temp);kill -TERM $pid || kill -KILL $pid
+# python3 server.py -p (value)
+done
+exit 0
