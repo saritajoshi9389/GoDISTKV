0417a499787cfc0016593d4f955d291e9d3213b6
akshayakhare <akshayakhare@gmail.com>
2017-11-18 04:40:38 +0000

    working automation

--- start_n_servers.sh
+++ start_n_servers.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 value=9000
 
-for number in {1..100}
+for number in {1..99}
 do
 temp=$(expr $value + $number)
-echo $temp
-# python3 server.py -p (value)
+# echo $temp
+python3 server.py -p $temp &
 done
+python3 server.py -p 9100
 exit 0
 pid=$(lsof -i:9000 -t); kill -TERM $pid || kill -KILL $pid
 pid1=$(lsof -i:9001 -t); kill -TERM $pid1 || kill -KILL $pid1
